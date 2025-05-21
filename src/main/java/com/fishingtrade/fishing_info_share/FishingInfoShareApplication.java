113a19bbb141f1747bbd2a476fd2d55955ab978d
KENTARO KASHIO <contact@scriptwritemarket.com>
2025-05-21 15:31:26 +0900

    init

---
+++ src/main/java/com/fishingtrade/fishing_info_share/FishingInfoShareApplication.java
@@ -0,0 +1,13 @@
+package com.fishingtrade.fishing_info_share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingInfoShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingInfoShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fishingtrade/fishing_info_share/FishingInfoShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishingtrade.fishing_info_share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingInfoShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
